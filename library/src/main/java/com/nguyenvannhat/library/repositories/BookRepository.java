617eeae3205d346ff032efb17b712294d71b2875
NhatNguyen28122003 <127331215+NhatNguyen28122003@users.noreply.github.com>
2024-12-09 18:28:13 +0700

    Delete library directory

--- library/src/main/java/com/nguyenvannhat/library/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nguyenvannhat.library.repositories;
-
-import com.nguyenvannhat.library.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,Integer> {
-    Book findByName(String name);
-}
