d22ae51a008f75bb1a2ff65676efc9c241a60acf
NhatNguyen2812 <nhatdoden@gmail.com>
2024-12-20 16:57:45 +0700

    Merge remote-tracking branch 'origin/master' into dev

--- src/main/java/com/nguyenvannhat/library/components/CustomUserDetails.java
+++ src/main/java/com/nguyenvannhat/library/components/CustomUserDetails.java
@@ -9,15 +9,20 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Getter
 @Setter
-public class CustomUserDetails implements UserDetails {
-    private UserCustom userCustom;
-    private UserRepository userRepository;
+public class CustomUserDetails implements UserDetails, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private transient UserCustom userCustom;
+    private transient UserRepository userRepository;
 
     public CustomUserDetails(UserCustom userCustom, UserRepository userRepository) {
         this.userCustom = userCustom;
@@ -47,21 +52,23 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        Role role = userRepository.getRoleByUserName(userCustom.getUsername());
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
-        for (Object roleName : userRepository.getFunctionRoleName(role.getId())) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_" + roleName));
+        if (userRepository != null && userCustom != null) {
+            Role role = userRepository.getRoleByUserName(userCustom.getUsername());
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+            for (Object roleName : userRepository.getFunctionRoleName(role.getId())) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + roleName));
+            }
         }
         return authorities;
     }
 
     @Override
     public String getPassword() {
-        return userCustom.getPassword();
+        return userCustom != null ? userCustom.getPassword() : null;
     }
 
     @Override
     public String getUsername() {
-        return userCustom.getUsername();
+        return userCustom != null ? userCustom.getUsername() : null;
     }
 }
